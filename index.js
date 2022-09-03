41ef8f7c3680e5ebc451ade2dd172f709e08ae30
Onuoha Collins <onuohacollinschidubem@gmail.com>
2022-09-03 20:25:14 +0100

    initializing a new project

---
+++ index.js
@@ -0,0 +1,16 @@
+function addName(num1, num2) {
+    return num1 * num2;
+}
+
+console.log('====================================');
+console.log(addName(23, 10));
+console.log('====================================');
+
+
+function getName(name) {
+    return `hi my name is  ${name}`
+}
+
+console.log('====================================');
+console.log(getName('collins'));
+console.log('====================================');
